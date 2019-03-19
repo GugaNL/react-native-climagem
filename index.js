59ab1e7ee2535b5dffdd46d2cf708375b5501a43
Gustavo Nunes Lucena <gustavonuneslucena@Gustavos-MacBook-Air.local>
2019-03-18 22:22:08 -0300

    Add es6-symbol package and imports in indexjs

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import {AppRegistry} from 'react-native';
 import App from './src/main/App';
 import {name as appName} from './app.json';
+global.Symbol = require('core-js/es6/symbol')
+require('core-js/fn/symbol/iterator')
+
+require('core-js/fn/map')
+require('core-js/fn/set')
+require('core-js/fn/array/find')
+
 
 AppRegistry.registerComponent(appName, () => App);
